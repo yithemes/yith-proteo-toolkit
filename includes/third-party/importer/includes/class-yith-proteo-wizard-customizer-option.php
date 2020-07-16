bc939c3cc0f99b5e21656f112971554e56b30e01
francgrasso <francgrasso@gmail.com>
2020-07-16 15:58:11 +0200

    fix customizer importer class

--- includes/third-party/importer/includes/class-yith-proteo-wizard-customizer-option.php
+++ includes/third-party/importer/includes/class-yith-proteo-wizard-customizer-option.php
@@ -1,13 +1,15 @@
 <?php
 /**
- * A class that extends WP_Customize_Setting so we can access
- * the protected updated method when importing options.
  *
- * Used in the Customizer importer.
+ * Customizer importer.
  *
  * @package YITH_Proteo_tookit
  */
 
+/**
+ * A class that extends WP_Customize_Setting so we can access
+ * the protected updated method when importing options.
+ */
 final class YITH_Proteo_Wizard_Customizer_Option extends \WP_Customize_Setting {
 	/**
 	 * Import an option value for this setting.
